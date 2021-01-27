fa5f9070a43afb9eae5c2faccc8d7fe0e528e788
Zoe Meng <zoebo1225@gmail.com>
2021-01-26 20:12:30 -0800

    Broken into homepage into smaller components, finished styling sheet

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -5,13 +5,19 @@ import './menu-item.style.scss';
 
 const MenuItem = ({title, imageUrl, size}) => {
   return(
-    <div style = {{backgroundImage: `url(${imageUrl})` }} 
-    className = {`${size} menu-item`}>
-      <div className ='content'>
-          <h1 className = 'title'>{title.toUpperCase()}</h1>
-          <span className = 'subtitle'>SHOP NOW</span>
+    <div className = {`${size} menu-item`}>
+      <div 
+        className = 'background-image' 
+        style = {{backgroundImage: `url(${imageUrl})`
+      }}
+      />
+
+
+        <div className ='content'>
+            <h1 className = 'title'>{title.toUpperCase()}</h1>
+            <span className = 'subtitle'>SHOP NOW</span>
+        </div>
       </div>
-    </div>
   )
 }
 
