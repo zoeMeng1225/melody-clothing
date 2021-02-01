daa11274adbd6168fad1f56d929fa8dbf9cdbf60
zoeMeng1225 <46607720+zoeMeng1225@users.noreply.github.com>
2021-02-01 13:36:51 -0800

    Update firebase.util.js

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -5,13 +5,7 @@ import 'firebase/auth';
 
 
 const config = {
-  apiKey: "AIzaSyAX_uU2-sg3L06md9OAxwoMoGcAw0k-pzg",
-  authDomain: "memory-clothing.firebaseapp.com",
-  projectId: "memory-clothing",
-  storageBucket: "memory-clothing.appspot.com",
-  messagingSenderId: "1024158026697",
-  appId: "1:1024158026697:web:30b8c30da6130aa5587618",
-  measurementId: "G-GHKLG1CJ1M"
+    // input SDK in your firebase
 };
 
 
